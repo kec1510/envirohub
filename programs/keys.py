cf643d14bfb8879df2f60597a3a31e861a7f40c4
Karina Chung <62352820+kec1510@users.noreply.github.com>
2023-12-22 10:46:54 -0500

    Delete programs/keys.py

--- programs/keys.py
+++
@@ -1,9 +0,0 @@
-class NYT:
-    api_key = '55io9aooAhWw1lEPCnbYhkdArHrF8zJ9'
-    
-class OpenWeather:
-    api_key = 'ba5fb8bd88d2eb5df70f0af420742973'
-    api_key_2 = 'bf82db2c09d6eb69e6d7428f0612e892'
-
-class NewsAPI:
-    api_key = 'aedbaf4d10e64fa0ac0a8d199285fbab'
